f8ab0057c929f024687ef114b6aed97bd1a83cd7
hoanganhchimxanh <155616612+hoanganhchimxanh@users.noreply.github.com>
2025-12-12 10:36:12 +0700

    Network model fix

--- backend/models/network.model.js
+++ backend/models/network.model.js
@@ -2,30 +2,110 @@ const mongoose = require("mongoose");
 
 const networkSchema = new mongoose.Schema(
   {
-    name: { type: String, required: true },
-
-    status: {
-      type: String,
-      enum: ["ACTIVE", "PROCESSING", "INACTIVE"],
-      default: "ACTIVE",
+    // Thông tin nhân viên quản lý
+    assignedUser: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
     },
 
-    // Kênh chính của Network (brand account chính)
+    // Lịch kiểm tra (admin reminder)
+    reminderDate: {
+      type: Date,
+      default: null,
+    },
+
+    // Thông tin Profile AdSense
+    profileAdsenseId: {
+      type: String,
+      required: true,
+      unique: true, // Mã ID duy nhất
+    },
+
+    // Email của profile AdSense
+    emailAddress: {
+      type: String,
+      required: true,
+      lowercase: true,
+      trim: true,
+    },
+
+    // Email khôi phục
+    recoveryEmail: {
+      type: String,
+      lowercase: true,
+      trim: true,
+      default: "",
+    },
+
+    // Ngày tạo của profile AdSense
+    creationDate: {
+      type: Date,
+      required: true,
+    },
+
+    // Thông tin thuế
+    taxName: {
+      type: String,
+      default: "",
+    },
+
+    // Vị trí làm việc
+    location: {
+      type: String,
+      enum: ["HOME", "OFFICE", "OTHER"],
+      default: "OFFICE",
+    },
+
+    // Thông tin kênh chính
     mainChannel: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "Channel",
-      required: true,
+      default: null,
     },
 
-    // Tài khoản YouTube chính của network (primary owner)
-    primaryAccountEmail: {
+    // Link kênh
+    linkedChannelUrl: {
       type: String,
-      required: true,
+      default: "",
     },
 
-    note: { type: String },
+    // Email brand account của kênh
+    emailChannel: {
+      type: String,
+      lowercase: true,
+      trim: true,
+      default: "",
+    },
+
+    // Ngày tạo kênh
+    channelJoinDate: {
+      type: Date,
+      default: null,
+    },
+
+    // Quốc gia
+    country: {
+      type: String,
+      default: "VN",
+    },
+
+    // Trạng thái
+    status: {
+      type: String,
+      enum: ["ACTIVE", "PROCESSING", "INACTIVE", "LOCKED"],
+      default: "ACTIVE",
+    },
+
+    // Ghi chú bổ sung
+    note: {
+      type: String,
+      default: "",
+    },
   },
-  { timestamps: true }
+  {
+    timestamps: true,
+  }
 );
 
 module.exports = mongoose.model("Network", networkSchema);
